ba66b50f19fe6bc362886d6b93ade45eb03b2299
Ricky <www.rishabhrawat570@gmail.com>
2018-06-13 21:19:10 +0530

    Change redirect URI.

--- config.php
+++ config.php
@@ -5,6 +5,6 @@
 	$gClient->setClientId("357483147160-5fmfh4fb9eq80cf218a9niubes9l1618.apps.googleusercontent.com");
 	$gClient->setClientSecret("88HOXQlkBfR9e15tIFGXrbnp");
 	$gClient->setApplicationName("Click to Call");
-	$gClient->setRedirectUri("https://safe-woodland-15391.herokuapp.com/g-callback.php");
+	$gClient->setRedirectUri("https://secure-taiga-33284.herokuapp.com/g-callback.php");
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 ?>
